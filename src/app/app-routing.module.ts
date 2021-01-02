a6802642a9e15fa7bd695af2c904556bf7ee20a4
dev.darshana991 <dev.darshana92@gmail.com>
2021-01-02 08:56:54 +0530

    auth form added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './project/auth/auth.component';
 import { EmpSearchComponent } from './project/emp-search/emp-search.component';
 import { EmployeeComponent } from './project/employee/employee.component';
 
 const routes: Routes = [
   {path: '' , component: EmployeeComponent},
-  {path: 'search' , component: EmpSearchComponent}
+  {path: 'search' , component: EmpSearchComponent},
+  {path: 'login' , component: AuthComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './project/employee/employee.component';
 import { EmpSearchComponent } from './project/emp-search/emp-search.component';
-
+import { AuthComponent } from './project/auth/auth.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
-    EmpSearchComponent
+    EmpSearchComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
